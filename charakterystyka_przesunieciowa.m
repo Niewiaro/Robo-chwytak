6dd0ce37a427ae01fb5b72aba9b5611f02eaa11a
Niewiaro <kubuskrol@gmail.com>
2024-05-21 19:56:39 +0200

    offset i zmiana na charakterystykę od wysuwu

--- charakterystyka_przesunieciowa.m
+++ charakterystyka_przesunieciowa.m
@@ -18,7 +18,8 @@ l2 = 124;
 l3 = 50;
 
 % x = 0:0.01:180;
-x = 165.6:0.01:171.8;
+offset = 9;
+x = 165.6+offset:0.01:171.8+offset;
 
 %% Oś Ox
 
@@ -30,10 +31,10 @@ c = ((x.^2 - l1.^2 - l2.^2 - l3.^2) / (2*l3)) - l1.^2;
 % make_plot(x, y_positive, 'x', 'y', 'Charakterystyka przesunięciowa dla osi Ox', '+∆')
 
 y_negative = (l2 * (-b - sqrt(b.^2 - 4*a*c))) / (2*a); % poprawne roziązanie
-make_plot(x, y_negative, 'x', 'y', 'Charakterystyka przesunięciowa dla osi Ox', '-∆')
+make_plot(x - x(1), y_negative, 'x', 'y', 'Charakterystyka przesunięciowa dla osi Ox', '-∆')
 
 y_negative_diff = (l2.*(-(2.*l2.^2.*x.*(x.^2-l3.^2-l2.^2-l1.^2))/(l3.^2.*sqrt((l2.^2.*((x.^2-l3.^2-l2.^2-l1.^2)).^2)/(l3.^2)-4.*(l2.^2+l1.^2).*((x.^2-l3.^2-l2.^2-l1.^2)/(2.*l3)-l1.^2)))+(2.*(l2.^2+l1.^2).*x)/(l3.*sqrt((l2.^2.*((x.^2-l3.^2-l2.^2-l1.^2)).^2)/(l3.^2)-4.*(l2.^2+l1.^2).*((x.^2-l3.^2-l2.^2-l1.^2)/(2.*l3)-l1.^2)))-(2.*l2.*x)/(l3)))/(2.*(l2.^2+l1.^2));
-make_plot(x, y_negative_diff, 'x', 'y', 'Charakterystyka prędkościowa dla osi Ox', '-∆')
+make_plot(x - x(1), y_negative_diff, 'x', 'y', 'Charakterystyka prędkościowa dla osi Ox', '-∆')
 
 %% Oś Oy
 
@@ -42,10 +43,10 @@ b = (l1/l3) * (x.^2 - l1.^2 - l2.^2 - l3.^2);
 c = ((x.^2 - l1.^2 - l2.^2 - l3.^2) / (2*l3)) - l2.^2;
 
 y_positive = (l2 * (-b + sqrt(b.^2 - 4*a*c))) / (2*a); % poprawne roziązanie
-make_plot(x, y_positive, 'x', 'y', 'Charakterystyka przesunięciowa dla osi Oy', '+∆')
+make_plot(x - x(1), y_positive, 'x', 'y', 'Charakterystyka przesunięciowa dla osi Oy', '+∆')
 
 % y_negative = (l2 * (-b - sqrt(b.^2 - 4*a*c))) / (2*a);
 % make_plot(x, y_negative, 'x', 'y', 'Charakterystyka przesunięciowa dla osi Oy', '-∆')
 
 y_positive_diff = (l2.*(-(2.*l1.^2.*x.*(x.^2-l3.^2-l2.^2-l1.^2))/(l3.^2.*sqrt((l1.^2.*((x.^2-l3.^2-l2.^2-l1.^2)).^2)/(l3.^2)-4.*(l2.^2+l1.^2).*((x.^2-l3.^2-l2.^2-l1.^2)/(2.*l3)-l2.^2)))+(2.*(l2.^2+l1.^2).*x)/(l3.*sqrt((l1.^2.*((x.^2-l3.^2-l2.^2-l1.^2)).^2)/(l3.^2)-4.*(l2.^2+l1.^2).*((x.^2-l3.^2-l2.^2-l1.^2)/(2.*l3)-l2.^2)))-(2.*l1.*x)/(l3)))/(2.*(l2.^2+l1.^2));
-make_plot(x, y_positive_diff, 'x', 'y', 'Charakterystyka prędkościowa dla osi Oy', '+∆')
+make_plot(x - x(1), y_positive_diff, 'x', 'y', 'Charakterystyka prędkościowa dla osi Oy', '+∆')
